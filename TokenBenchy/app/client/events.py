b4157e97a916b6a8ce2f618671236104e5042406
CTCycle <thomas.virdis89@gmail.com>
2025-08-26 09:21:30 +0200

    decoupled serializer from low-level methods

--- TokenBenchy/app/client/events.py
+++ TokenBenchy/app/client/events.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 from TokenBenchy.app.utils.data.serializer import DataSerializer
 from TokenBenchy.app.utils.downloads import DatasetManager, TokenizersDownloadManager
@@ -15,6 +14,7 @@ from TokenBenchy.app.logger import logger
 class DatasetEvents:
 
     def __init__(self, configuration, hf_access_token):
+        self.serializer = DataSerializer()
         self.configuration = configuration
         self.hf_access_token = hf_access_token           
            
@@ -48,29 +48,33 @@ class DatasetEvents:
              'AVG_words_length': [np.nan] * len(documents),
              'STD_words_length': [np.nan] * len(documents)})
         
-        # serialize text dataset by saving it into database
-        serializer = DataSerializer(self.configuration)        
-        serializer.save_text_dataset(text_dataset)  
+        # serialize text dataset by saving it into database   
+        # TO DO: change database assignation        
+        self.serializer.save_text_dataset(text_dataset)  
 
         # check thread for interruption         
         update_progress_callback(3, 3, progress_callback)
 
-        return dataset_name
-        
+        return dataset_name        
 
 
 ###############################################################################
 class BenchmarkEvents:
 
-    def __init__(self, configuration, hf_access_token): 
+    def __init__(self, configuration, hf_access_token):
+        self.serializer = DataSerializer() 
         self.configuration = configuration    
         self.hf_access_token = hf_access_token                                   
            
     #--------------------------------------------------------------------------
-    def run_dataset_evaluation_pipeline(self, progress_callback=None, worker=None):        
-        benchmarker = BenchmarkTokenizers(self.configuration)
-        benchmarker.calculate_text_statistics(
-            progress_callback=progress_callback, worker=worker)         
+    def run_dataset_evaluation_pipeline(self, progress_callback=None, worker=None): 
+        text_dataset = self.serializer.load_text_dataset()
+        benchmarker = BenchmarkTokenizers(self.configuration)         
+        documents = benchmarker.calculate_text_statistics(text_dataset,
+            progress_callback=progress_callback, worker=worker)
+
+        # save dataset statistics through upserting into the the text dataset table
+        self.serializer.save_dataset_statistics(documents)         
     
     #--------------------------------------------------------------------------
     def get_tokenizer_identifiers(self, limit=1000, worker=None):
@@ -84,8 +88,15 @@ class BenchmarkEvents:
         benchmarker = BenchmarkTokenizers(self.configuration)
         downloader = TokenizersDownloadManager(self.configuration, self.hf_access_token)
         tokenizers = downloader.tokenizer_download(worker=worker)
-        results = benchmarker.run_tokenizer_benchmarks(
+        vocabularies, vocab_stats, benchmarks, NSL = benchmarker.run_tokenizer_benchmarks(
            tokenizers, progress_callback=progress_callback, worker=worker) 
+        # save results into database
+        self.serializer.save_benchmark_results(benchmarks)
+        self.serializer.save_vocabulary_statistics(vocab_stats)
+        self.serializer.save_NSL_benchmark(NSL) if NSL else None
+        for voc in vocabularies:
+            self.serializer.save_vocabulary_tokens(voc)
+
 
         return tokenizers     
 
@@ -94,8 +105,9 @@ class BenchmarkEvents:
 class VisualizationEnvents:
 
     def __init__(self, configuration : dict):
+        self.serializer = DataSerializer()         
+        self.img_resolution = 400
         self.configuration = configuration 
-        self.DPI = configuration.get('image_resolution', 400)
 
     #--------------------------------------------------------------------------
     def visualize_benchmark_results(self, worker=None, progress_callback=None):
@@ -105,10 +117,12 @@ class VisualizationEnvents:
         figures.append(visualizer.plot_vocabulary_size())
         check_thread_status(worker)
         update_progress_callback(1, 3, progress_callback)
+
         # 2. generate plot of token length distribution
         figures.extend(visualizer.plot_tokens_length_distribution())
         check_thread_status(worker)
-        update_progress_callback(2, 3, progress_callback)        
+        update_progress_callback(2, 3, progress_callback) 
+
         # 2. generate plot of words versus subwords
         figures.append(visualizer.plot_subwords_vs_words())    
         update_progress_callback(3, 3, progress_callback)        
